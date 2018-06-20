ffe83195f0bda1edeafb3f1fccfc026b2f7ce994
donasktello <shvetsov1988@gmail.com>
2018-06-20 17:16:34 +0300

    Fix of paths

--- startup.sh
+++ startup.sh
@@ -2,8 +2,8 @@
 sudo -u appuser bash - <<EOF
 cd /home/appuser
 git clone -b cloud-testapp  https://github.com/Otus-DevOps-2018-05/donasktello_infra.git automation && \
-sudo ./automation/config-scripts/install_ruby.sh && \
-sudo ./automation/config-scripts/install_mongodb.sh && \
+sudo ./automation/install_ruby.sh && \
+sudo ./automation/install_mongodb.sh && \
 ./automation/config-scripts/deploy.sh
 rm -Rfv automation
 EOF
